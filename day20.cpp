8f9df2bd2abb236d41f3d30f24a8f1391e50287d
Simon Heuser <shsr04@users.noreply.github.com>
2019-12-21 09:25:25 +0100

    Day 20 (done!)

--- day20.cpp
+++ day20.cpp
@@ -37,7 +37,7 @@ optional<coord> get_adjacent_period(map<coord, char> const &tiles, coord p) {
     return {};
 }
 
-bool are_grouped(coord a, coord b) {
+bool are_adjacent(coord a, coord b) {
     vector<coord> neighbors = {
         a, {a.x, a.y + 1}, {a.x, a.y - 1}, {a.x + 1, a.y}, {a.x - 1, a.y}};
     for (coord &v : neighbors)
@@ -71,34 +71,42 @@ void print_grid(map<coord, char> const &tiles, coord marker, char symbol) {
     }
 }
 
-/// Get the pair's coordinate, but excluding the `exclude` coordinate.
-/// This is used to find the corresponding other label of a label.
-optional<coord> get_corresponding_pair(map<coord, char> const &tiles,
-                                       coord exclude, char first, char second) {
-    auto label = r::find_if(tiles, [first, second, exclude, &tiles](auto &x) {
-        return !are_grouped(x.first, exclude) && x.second == first &&
-               tiles.at(get_adjacent_uppercase(tiles, x.first).value()) ==
-                   second;
-    });
-    if (label == tiles.end())
+optional<string> get_label_string(map<coord, char> const &tiles,
+                                  coord teleporter) {
+    if (!isupper(tiles.at(teleporter)))
         return {};
-
-    auto path_tile = get_adjacent_period(tiles, label->first);
-    if (!path_tile)
-        path_tile = get_adjacent_period(
-            tiles, get_adjacent_uppercase(tiles, label->first).value());
-    return path_tile.value();
-}
-
-/// @param teleporter One end of one side of a teleporter (e.g. the `B` tile of
-/// a BC teleporter)
-optional<coord> get_other_end(map<coord, char> const &tiles, coord teleporter) {
     auto first = teleporter,
          second = get_adjacent_uppercase(tiles, teleporter).value();
     if (first.x > second.x || first.y > second.y)
         swap(first, second);
-    auto other_end = get_corresponding_pair(tiles, teleporter, tiles.at(first),
-                                            tiles.at(second));
+    return {{tiles.at(first), tiles.at(second)}};
+}
+
+/// Get the pair's coordinate, but excluding the `exclude` coordinate.
+/// This is used to find the corresponding other label of a label.
+optional<coord> get_corresponding_path(map<coord, char> const &tiles,
+                                       coord exclude, string label) {
+    /// IMPORTANT: get_label_string must be used here because it will already
+    /// sort the label correctly. Otherwise, two equal labels could compare
+    /// non-equal if ordered unluckily.
+    auto other_label = r::find_if(tiles, [&](auto &x) {
+        return !are_adjacent(x.first, exclude) &&
+               get_label_string(tiles, x.first) == label;
+    });
+    if (other_label == tiles.end())
+        return {};
+
+    auto path_tile = get_adjacent_period(tiles, other_label->first);
+    if (!path_tile)
+        path_tile = get_adjacent_period(
+            tiles, get_adjacent_uppercase(tiles, other_label->first).value());
+    return path_tile.value();
+}
+
+/// Get the open tile next to the other end of the given teleporter.
+optional<coord> get_other_end(map<coord, char> const &tiles, coord teleporter) {
+    auto label = get_label_string(tiles, teleporter).value();
+    auto other_end = get_corresponding_path(tiles, teleporter, label);
     return other_end;
 }
 
@@ -119,6 +127,8 @@ optional<int> distance(map<coord, char> const &tiles, coord from, coord to) {
         auto [u, level, dist] = q.front();
         q.pop_front();
         // cout << u << " L" << level << "\n";
+        if (level > 25)
+            continue;
         if (u == to && level == 0)
             return dist;
 
@@ -131,12 +141,15 @@ optional<int> distance(map<coord, char> const &tiles, coord from, coord to) {
 
             auto l = level;
             if (isupper(tiles.at(v))) {
-                if (l == 0 && is_outer_edge(tiles, v))
+                auto is_terminal = get_label_string(tiles, v) == "AA" ||
+                                   get_label_string(tiles, v) == "ZZ";
+                if (is_outer_edge(tiles, v) &&
+                    ((l == 0 && !is_terminal) || (l > 0 && is_terminal))) {
                     continue;
+                }
                 auto w = get_other_end(tiles, v);
                 if (!w)
                     continue;
-                visited.insert({v, l});
                 l += is_outer_edge(tiles, v) ? -1 : 1;
                 v = w.value();
             }
@@ -168,8 +181,8 @@ int main(int argc, char **argv) {
         c_tile.y++;
         dimensions.y = max(c_tile.y, dimensions.y);
     }
-    auto start = get_corresponding_pair(tiles, {0, 0}, 'A', 'A').value(),
-         end = get_corresponding_pair(tiles, {0, 0}, 'Z', 'Z').value();
+    auto start = get_corresponding_path(tiles, {0, 0}, "AA").value(),
+         end = get_corresponding_path(tiles, {0, 0}, "ZZ").value();
     cout << "Start: " << start << ", End: " << end << "\n";
     cout << distance(tiles, start, end).value() << "\n";
 }
